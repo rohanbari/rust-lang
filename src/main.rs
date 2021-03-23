3c5551414bbf950d01507f7fd256249410651605
rohanbari <rohanbari4@gmail.com>
2021-03-24 04:22:48 +0530

    Guess the Game project
      1. While working on this project, understood various syntaxes of Rust
      2. More changes in this project are expected to happen in the future

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("=== Guess The Number Game ===");
+
+    // Using Rng trait to generate unique random numbers
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    print!("Please input your guess. ");
+
+    // Accepting user input as a string
+    let mut guess = String::new();
+
+    // Reading the input, some notables things:
+    //   - The parameter  guess  is passed as a mutable-reference
+    //   - The variables passed as arguments are considered immutable objects
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    print!("You guessed {} ", guess);
+
+    // Chain method call, trimming the whitespaces, then parsing and handling
+    // any possible error that might happen during parse operation
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    // Comparing the  secret_number  with  guess  by using Ordering trait
+    match guess.cmp(&secret_number) {
+        Ordering::Greater => println!("Greater than expected."),
+        Ordering::Less => println!("Lesser than expected."),
+        Ordering::Equal => println!("CORRECT!")
+    };
+
+    println!("The number was {}", secret_number);
 }
